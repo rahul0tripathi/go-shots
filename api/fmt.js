84e40bbee049cf09b62e3223b137a6c273097e42
rahul0tripathi <rahultripathidev@gmail.com>
2022-10-19 13:15:45 +0530

    bg: --

--- api/fmt.js
+++ api/fmt.js
@@ -1,17 +1,17 @@
 const fetch = require('node-fetch')
 
 export default async function fmt(request, response) {
-  console.log('called')
   const resp = await fetch('https://go.dev/_/fmt?backend=', {
     method: 'POST',
     headers: {
       'content-type': 'application/x-www-form-urlencoded'
     },
-    body: new URLSearchParams({
+    body: {
       body: request.body.code,
       imports: true
-    })
+    }
   })
+  console.log(resp, resp.json())
   const data = resp.json()
   response.status(200).json({
     err: data.Error,

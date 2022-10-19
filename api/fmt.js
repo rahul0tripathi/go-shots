db988d89bcb2c2e7689ef619115231d327a5de89
rahul0tripathi <rahultripathidev@gmail.com>
2022-10-19 12:23:37 +0530

    ft: upgraded to node-fetch

--- api/fmt.js
+++ api/fmt.js
@@ -1,20 +1,19 @@
-const axios = require('axios')
-
+import fetch from 'node-fetch'
 export default async function fmt(request, response) {
-  const resp = await axios.post(
-    'https://go.dev/_/fmt?backend=',
-    {
+  console.log('called')
+  const resp = await fetch('https://go.dev/_/fmt?backend=', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
       body: request.body.code,
       imports: true
-    },
-    {
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded'
-      }
-    }
-  )
+    })
+  })
+  const data = response.json()
   response.status(200).json({
-    err: resp.data.Error,
-    body: resp.data.Body
+    err: data.Error,
+    body: data.Body
   })
 }

29ef7c5e375bb0afa9e6f745ad0f60303faa7266
rahul0tripathi <rahultripathidev@gmail.com>
2022-10-19 13:01:07 +0530

    bg: --

--- api/run.js
+++ api/run.js
@@ -1,21 +1,20 @@
-const axios = require('axios')
+const fetch = require('node-fetch')
 
 export default async function run(request, response) {
-  const resp = await axios.post(
-    'https://go.dev/_/compile?backend=',
-    {
+  const resp = await fetch('https://go.dev/_/compile?backend=', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
       body: request.body.code,
       version: 2,
       withVet: true
-    },
-    {
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded'
-      }
-    }
-  )
+    })
+  })
+  const data = resp.json()
   response.status(200).json({
-    err: resp.data.Errors,
-    events: resp.data.Events
+    err: data.Errors,
+    body: data.Events
   })
 }
